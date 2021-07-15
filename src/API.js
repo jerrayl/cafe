b1b11e7058b45d6c5525fc23e4ee58eac2fa91e3
Jerrayl <jerrayl.ng@psychpress.com.au>
2021-07-15 13:31:18 +1000

    add files

---
+++ src/API.js
@@ -0,0 +1,13 @@
+const images = {
+  "apple": "https://healthjade.com/wp-content/uploads/2017/10/apple-fruit.jpg",
+  "orange": "https://media.istockphoto.com/photos/orange-picture-id185284489?k=6&m=185284489&s=612x612&w=0&h=x_w4oMnanMTQ5KtSNjSNDdiVaSrlxM4om-3PQTIzFaY=",
+  "banana":
+    "https://cdn.mos.cms.futurecdn.net/42E9as7NaTaAi4A6JcuFwG-1200-80.jpg"
+  ,
+  "kiwi": "https://images.immediate.co.uk/production/volatile/sites/30/2020/02/Kiwi-fruits-582a07b.jpg?quality=90&resize=504,458"
+}
+
+export const getImage = (orderChoice) => {
+  return images[orderChoice];
+}
+
---
+++ src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+import { getImage } from "./API.js"
+import React, { useState } from 'react';
+
+function App() {
+  const [name, setName] = useState("");
+  const [orderChoice, setOrderChoice] = useState("apple");
+  
+  const [orders, setOrders] = useState([]);
+
+  const onSubmitButtonClick = () => {
+    setOrders(oldOrders => [...oldOrders, {
+      "name": name,
+      "orderChoice": orderChoice
+    }]);
+  }
+
+  return (
+    <div className="App">
+      <h2>Order Page</h2>
+      <div className="split">
+        <div>
+          <div className="row">
+            <label for="name">Enter your name:</label>
+            <input value={name} onChange={(e) => setName(e.target.value)}></input>
+          </div>
+          <div className="row">
+            <label for="menu">Choose a dish:</label>
+            <select name="menu" value={orderChoice} onInput={(e) => setOrderChoice(e.target.value)}>
+              <option value="apple">Apple</option>
+              <option value="orange">Orange</option>
+              <option value="banana">Banana</option>
+              <option value="kiwi">Kiwi</option>
+            </select>
+          </div>
+          <div className="row">
+            <button onClick={onSubmitButtonClick}>Submit</button>
+          </div>
+        </div>
+        <div>
+          <h3>Your name is: {name}</h3>
+          <img className="displayImage" src={getImage(orderChoice)} alt="foodImage" />
+        </div>
+      </div>
+      <h2>Admin Page</h2>
+      {orders.map(order => <span className="row"><p>D</p><p className="bold marginRight">{order.name}</p><p>{order.orderChoice}</p></span>)}
+    </div>
+  );
+}
+
+export default App;
